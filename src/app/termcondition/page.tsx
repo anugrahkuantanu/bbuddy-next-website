d3c69485b9654d311c81a43e908626497a4a8eb8
anugrahkuantanu <anugrahkuantanu@gmail.com>
2023-10-28 15:47:47 +0200

    term condition

---
+++ src/app/termcondition/page.tsx
@@ -0,0 +1,202 @@
+
+
+type Props = {}
+const TermConditionPage = (props: Props) => {
+  return (
+    <section id="termandcondition" className=" pt-0 pb-16">
+      {/* HEADINGS */}
+      <div>
+        <div>
+          <p className=" text-center font-semibold text-2xl mb-5">
+            Term and condition
+          </p>
+          <div className="flex justify-center mt-5">
+          </div>
+        </div>
+      </div>
+
+        <div className="text-blue p-0 md:p-4 text-left mb-150 rounded-[20px]">
+          <p className=" text-1xl mb-1">
+          This privacy notice for BBuddy describes how and why we might collect, store, use, and/or process your information when you use our services such as when you:</p>
+          <div className="p-5">
+            <ul className="list-disc list-inside">
+                  <li className=" text-1xl">Download and use our application.</li>
+                  <li className=" text-1xl">Visit our website.</li>
+                  <li className=" text-1xl mb-1">Engage with us in other related ways ― including any sales, marketing, or events.</li>
+                </ul>
+            </div>
+          <p className=" text-1xl mb-5">
+          Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services. 
+          If you still have any questions or concerns, please contact us at <a className="font-bold" href="mailto:info@bbuddy.ai">info@bbuddy.ai</a> </p>
+          <div className="p-0 md:p-5">
+            <p className=" text-2xl font-bold mb-5">
+                1. Personal Information
+            </p>
+            <p className=" text-1xl">
+              When you visit, use, or navigate our services, we may process personal information depending on how you interact with Bbuddy and the services, 
+              choices you make, and the products and features you use. We collect personal information that you voluntarily provide to us when you download our app, express an interest 
+              in obtaining information about us or our products and Services, when you participate in activities on the Services, or otherwise when you contact us.</p>
+            <p className=" text-1xl mb-1">
+              The personal information that we collect depends on the context of your interactions with us and the Services, the choices you make, and the products and features you use. 
+              The personal information we collect may include the following:</p>
+            <div className="p-2 md:p-5">
+            <ul className="list-disc list-inside">
+                <li className=" text-1xl">Name</li>
+                <li className=" text-1xl">E-mail address</li>
+                <li className=" text-1xl mb-1">Age</li>
+              </ul>
+            </div>
+
+            <p className=" text-1xl mb-5">
+              All personal information that you provide to us must be true, complete, and accurate, so is important to actualize it when necessary. 
+            </p>
+
+
+            <p className=" text-2xl font-bold mb-5">
+            	2. Sensitive personal information
+            </p>
+            <p className=" text-1xl mb-5">
+              We may process sensitive personal information, when necessary, with your consent or as otherwise permitted by applicable law. 
+            </p>
+            <p className=" text-2xl font-bold mb-5">
+            	3. Information Processing</p>
+            <p className=" text-1xl mb-5">
+            We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, 
+            and to comply with law. We may also process your information for other purposes with your consent. 
+            We process your information only when we have a valid legal reason to do so.</p>
+            <h1 className=" font-bold text-2xl mb-5">
+              4. Security
+            </h1>
+            <p className=" text-1xl mb-5">
+            We have implemented appropriate and reasonable technical and organizational security measures designed to protect the security of any 
+            personal information we process. However, despite our safeguards and efforts to secure your information, no electronic transmission over 
+            the Internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, 
+            cybercriminals, or other unauthorized third parties will not be able to defeat our security and improperly collect, access, steal, or modify
+             your information. Although we will do our best to protect your personal information, transmission of personal information to and from our Services 
+             is at your own risk. You should only access the Services within a secure environment.</p>
+            <h1 className=" font-bold text-2xl mb-5">
+              5. Account Information
+            </h1>
+            <p className=" text-1xl mb-1">
+            If you would at any time like to review or change the information in your account or terminate your account, you can:
+            </p>
+            <div className="p-2 md:p-5">
+            <ul className="list-disc list-inside">
+                <li className=" text-1xl">Log in to your account settings and update your user account.</li>
+                <li className=" text-1xl">Contact us using the contact information provided.</li>
+                <li className=" text-1xl mb-5">Others.</li>
+              </ul>
+
+            </div>
+            
+            <p className=" text-1xl mb-5">
+            Upon your request to terminate your account, we will deactivate or delete your account and information from our active databases. However, 
+            we may retain some information in our files to prevent fraud, troubleshoot problems, assist with any investigations, enforce our legal terms 
+            and/or comply with applicable legal requirements.
+            </p>
+
+            <p className=" text-2xl font-bold mb-5">
+            	6. Data Retention
+            </p>
+            <p className=" text-1xl mb-5">
+            We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice unless a longer retention 
+            period is required or permitted by law. When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize such information.
+            
+            </p>
+            <p className=" text-2xl font-bold mb-5">
+            	7. Use of AI bot
+            </p>
+            <p className=" text-1xl mb-5">
+            We use the Chat GPT API for our AI bot, which processes user input to provide personalized responses. By using our Services, you consent to the collection and use of your information by the Chat GPT API.
+            </p>
+            <p className=" text-2xl font-bold mb-1">
+            	8. Application Data automatically collected
+            </p>
+            <p className=" text-1xl mb-1">
+            If you use our application(s), we also may collect the following information if you choose to provide us with access or permission:
+            </p>
+            <div className="p-2 md:p-5">
+              <ul className="list-disc list-inside">
+                  <li className=" text-1xl">Mobile Device Data. We automatically collect device information (such as your mobile device ID, model, and manufacturer), operating system, 
+                    version information and system configuration information, device and application identification numbers, browser type and version, hardware model 
+                    Internet service provider and/or mobile carrier, and Internet Protocol (IP) address (or proxy server). If you are using our application(s), we may 
+                    also collect information about the phone network associated with your mobile device, your mobile device’s operating system or platform, the type of 
+                    mobile device you use, your mobile device’s unique device ID, and information about the features of our application(s) you accessed.
+                  </li>
+                <li className=" text-1xl mb-1">Push Notifications. We may request to send you push notifications regarding your account or certain features of the application(s). If you wish to opt out from receiving these types of communications, you may turn them off in your device's settings.</li>
+                </ul>
+            </div>
+            <p className=" text-2xl font-bold mb-5">
+            	9. Information from minors
+            </p>
+            <p className=" text-1xl mb-5">
+            We do not knowingly solicit data from or market to children under 18 years of age. By using the Services, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such minor dependent’s use of the Services. 
+            If we learn that personal information from users less than 18 years of age has been collected, we will deactivate the account and take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from 
+            children under age 18, please contact us at <a className="font-bold" href="mailto:info@bbuddy.ai">info@bbuddy.ai</a>
+            </p>
+
+            <p className=" text-2xl font-bold mb-5">
+            	10. Withdrawing your consent
+            </p>
+            <p className=" text-1xl mb-5">
+            If we are relying on your consent to process your personal information, which may be express and/or implied consent depending on the applicable law, you have the right to withdraw your consent at any time. You can withdraw your consent at any time by contacting us.
+            </p>            
+            <p className=" text-2xl font-bold mb-5">
+              11. Updates
+            </p>
+            <p className=" text-1xl mb-5">
+            We may update this privacy notice from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either 
+            by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.</p>
+          </div>
+          <p className=" text-1xl mb-5">The General Data Protection Regulation (GDPR) and UK GDPR require us to explain the valid legal bases we rely on to process your personal information. As such, we may rely on the following legal bases to process your personal information:</p>
+          
+          <div className="p-0 md:p-5">
+            <p className=" text-1xl mb-5">
+              <span className="font-bold">consent. </span>
+              We may process your information if you have given us permission (i.e., consent) to use your personal information for a specific purpose. You can withdraw your consent at any time.
+            </p>              
+            <p className=" text-1xl mb-5">
+              <span className="font-bold">Performance of a Contract. </span>
+              We may process your personal information when we believe it is necessary to fulfill our contractual obligations to you, including providing our Services or at your request prior to entering a contract with you.
+            </p>              
+            <p className=" text-1xl mb-1">
+              <span className="font-bold">Legitimate Interests. </span>
+              We may process your information when we believe it is reasonably necessary to achieve our legitimate business interests and those interests do not outweigh your interests and fundamental rights and freedoms. For example, we may process your personal information for some of the purposes described to:
+            </p>
+              <div className="p-2 md:p-5">
+                <ul className="list-disc list-inside">
+                  <li className=" text-1xl">Send users information about special offers and discounts on our products and services.
+                  </li>
+                  <li className=" text-1xl">Develop and display personalized and relevant advertising content for our users.</li>
+                  <li className=" text-1xl">Analyze how our Services are used so we can improve them to engage and retain users.</li>
+                  <li className=" text-1xl">Support our marketing activities.</li>
+                  <li className=" text-1xl">Diagnose problems and/or prevent fraudulent activities.</li>
+                  <li className=" text-1xl mb-1">Understand how our users use our products and services so we can improve user experience.</li>
+                </ul>              
+              </div>            
+            <p className=" text-1xl mb-5">
+              <span className="font-bold">Legal Obligations. </span>
+              We may process your information where we believe it is necessary for compliance with our legal obligations, 
+              such as to cooperate with a law enforcement body or regulatory agency, exercise or defend our legal rights, or disclose your information as evidence in litigation in which we are involved.
+              </p>  
+            <p className=" text-1xl mb-5">
+              <span className="font-bold">Vital Interests. </span>
+              We may process your information where we believe it is necessary to protect your vital interests or the vital interests of a third party, such as situations involving potential threats to 
+              the safety of any person.
+            </p>  
+
+          </div>
+          <p className=" text-1xl mb-5">
+            In legal terms, we are generally the “data controller” under European data protection laws of the personal information described in this privacy notice since we determine the means and/or 
+            purposes of the data processing we perform. This privacy notice does not apply to the personal information we process as a “data processor” on behalf of our customers. In those situations, 
+            the customer that we provide services to and with whom we have entered into a data processing agreement is the “data controller” responsible for your personal information, and we merely process your information on their behalf in accordance with your instructions. If you want to know more about our customers' privacy practices, you should read their privacy policies and direct any questions you have to them.
+          </p>
+          <p className=" text-1xl mb-5 font-semibold">
+            Last update 27/09/2023
+          </p>
+        </div>
+    </section>
+  );
+};
+
+export default TermConditionPage;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,8 @@ const Footer = () => {
         </Link>
         <Link href="/privacy" className="mx-2 hover:underline">Privacy Policy
         </Link>
+        <Link href="/termcondition" className="mx-2 hover:underline">Term and condition
+        </Link>
         {/* <a className="mx-2 hover:underline">Contact Us</a>
         <a className="mx-2 hover:underline">Privacy Policy</a> */}
       </div>
@@ -16,3 +18,29 @@ const Footer = () => {
 };
 
 export default Footer;
+
+// import Link from 'next/link';
+
+// const Footer = () => {
+//   return (
+//     <footer className="fixed inset-x-0 bottom-0 bg-white dark:bg-gray-950 z-10 border-t border-zinc-300 py-2">
+//       <div className="container mx-auto flex justify-between items-center">
+//         <div>
+//           <Link href="/contact" className="hover:underline">
+//             Contact Us
+//           </Link>
+//         </div>
+//         <div className="flex justify-end">
+//           <Link href="/privacy" className="mx-2 hover:underline">
+//             Privacy Policy
+//           </Link>
+//           <Link href="/termcondition" className="mx-2 hover:underline">
+//             Terms and Conditions
+//           </Link>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
